fe0af299424d0341838dc14675849af6cb43228e
shibme <shibly.meeran@gmail.com>
2016-01-12 00:12:13 +0530

    Added reception of inline queries through updates

--- src/me/shib/java/lib/telegram/bot/types/Update.java
+++ src/me/shib/java/lib/telegram/bot/types/Update.java
@@ -4,6 +4,7 @@ public class Update {
 
     private long update_id;
     private Message message;
+    private InlineQuery inline_query;
 
     public long getUpdate_id() {
         return update_id;
@@ -13,9 +14,13 @@ public class Update {
         return message;
     }
 
+    public InlineQuery getInline_query() {
+        return inline_query;
+    }
+
     @Override
     public String toString() {
-        return "Update [update_id=" + update_id + ", message=" + message + "]";
+        return "Update [update_id=" + update_id + ", message=" + message + ", inline_query=" + inline_query + "]";
     }
 
 }
